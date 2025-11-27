5e76c31720ea8fc15af602df31fa89838e5f1c40
fdelabre-accor <frederic.delabre@consulting-for.accor.com>
2025-11-27 18:09:30 +0100

    Fix AgeBasedDiscount

--- src/javacore/chapter02/condition/exercise/AgeBasedDiscount.java
+++ src/javacore/chapter02/condition/exercise/AgeBasedDiscount.java
@@ -5,34 +5,29 @@ public class AgeBasedDiscount {
     public static void main(String[] args) {
 
         // -1, 0, 25, 26, 64, 65
-        int age = 25;
+        int age = 65;
         // -1, 0, 50
         double price = 50;
 
         double discountRate = 0;
         String displayAge = "Votre âge est de " + age + " an(s).";
 
-        if (age <= 25) {
-            discountRate  = 0.25;
-            price = price - (price * discountRate);
-        } else if (age >= 65) {
-            discountRate = 0.75;
-            price = price - (price * discountRate);
-        } else {
-        }
-
-        String discounRateMsg = "Vous bénéficiez d'une réduction de " + discountRate * 100 + "%.";
-        String priceMsg = "Le prix de votre billet de train est de " + price + "€.";
         String errMsg = "Le prix et/ou l'age ne peuvent pas être négatif. Prix: " + price + " , Age: " + age + ".";
 
         if (age < 0) {
             System.err.println(errMsg);
-        } else if(price < 0) {
+        } else if (price < 0) {
             System.err.println(errMsg);
         } else {
+            if (age <= 25) {
+                discountRate = 0.25;
+                price = price - (price * discountRate);
+            } else if (age >= 65) {
+                discountRate = 0.75;
+                price = price - (price * discountRate);
+            }
             System.out.println(displayAge);
-            System.out.println(discounRateMsg);
-            System.out.println(priceMsg);
+            System.out.println("Vous bénéficiez d'une réduction de " + discountRate * 100 + "%.Le prix de votre billet de train est de " + price + "€.");
         }
     }
 }
