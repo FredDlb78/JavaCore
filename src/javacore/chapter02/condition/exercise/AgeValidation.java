b87a3381aace84642f474c46f7624ee279cd84eb
fdelabre-accor <frederic.delabre@consulting-for.accor.com>
2025-12-02 23:22:12 +0100

    📝 Exercice : [⭐⭐] Vérification avancée d'un âge - Part 2

--- src/javacore/chapter02/condition/exercise/AgeValidation.java
+++ src/javacore/chapter02/condition/exercise/AgeValidation.java
@@ -11,9 +11,9 @@ public class AgeValidation {
         // Récupération de la date courante de la machine exécutant ce programme
         java.time.LocalDate currentDate = java.time.LocalDate.now();
 
-        int currentDay   = currentDate.getDayOfMonth(); // Jour courant (entre 1 et 31)
+        int currentDay = currentDate.getDayOfMonth(); // Jour courant (entre 1 et 31)
         int currentMonth = currentDate.getMonthValue(); // Mois courant (entre 1 et 12)
-        int currentYear  = currentDate.getYear();       // Année courante (202X)
+        int currentYear = currentDate.getYear();       // Année courante (202X)
 
         System.out.println("Nous sommes le " + currentDay + "/" + currentMonth + "/" + currentYear + ".");
 
@@ -25,35 +25,50 @@ public class AgeValidation {
 
         // Cas à tester : jours  au dessus de 31 et en dessous de 1, mois au dessus de 12 et en dessous de 1. 18 ans aujourd'hui, 18 ans demain et 18 ans hier
 
-        int birthdayDay = 2;
-        int birthdayMonth = 12;
-        int birthdayYear = 2007;
+        int birthdayDay = 28;
+        int birthdayMonth = 1;
+        int birthdayYear = 2008;
+
+        System.out.println("Vous êtes né le " + birthdayDay + "/" + birthdayMonth + "/" + birthdayYear + ".");
 
         final int LOW_LIMIT_BIRTHDAY_DAY = 1;
-        final int HIGH_LIMIT_BIRTHDAY_DAY = 31;
+        final int HIGH_LIMIT_BIRTHDAY_DAY = 31; // BONUS 2: cette valeur ne sera plus un final car elle devra être modifiée en fonction du mois
         final int LOW_LIMIT_BIRTHDAY_MONTH = 1;
         final int HIGH_LIMIT_BIRTHDAY_MONTH = 12;
         final int YEAR_OF_MAJORITY = 18;
 
+        int ageInYears = currentYear - birthdayYear;
+        int ageInMonths = currentMonth - birthdayMonth;
+        int ageInDays = currentDay - birthdayDay;
+
+        // BONUS 2 : cette vérification est impactée par la gestion des années bissextiles
+        // Actuellement, on vérifie juste que le jour est entre 1 et 31
+        // Si on prend en compte les années bissextiles, il faut remplacer la limite par un nombre de jours calculés en fonction du mois et de l'année
         if ((birthdayDay < LOW_LIMIT_BIRTHDAY_DAY || birthdayDay > HIGH_LIMIT_BIRTHDAY_DAY) || (birthdayMonth < LOW_LIMIT_BIRTHDAY_MONTH || birthdayMonth > HIGH_LIMIT_BIRTHDAY_MONTH)) {
             System.out.println("La combinaison jour/mois de votre anniversaire n'est pas valide.");
             System.out.println("Le jour doit être compris entre " + LOW_LIMIT_BIRTHDAY_DAY + " et " + HIGH_LIMIT_BIRTHDAY_DAY + ". Jour actuel : " + birthdayDay + ".");
             System.out.println("Le mois doit être compris entre " + LOW_LIMIT_BIRTHDAY_MONTH + " et " + HIGH_LIMIT_BIRTHDAY_MONTH + ". Mois actuel : " + birthdayMonth + ".");
+
         } else if ((currentYear - birthdayYear) > YEAR_OF_MAJORITY) {
             System.out.println("Vous êtes majeur.");
         } else if ((currentYear - birthdayYear) == YEAR_OF_MAJORITY && ((currentMonth - birthdayMonth) > 0)) {
             System.out.println("Vous êtes majeur.");
-        } else if ((currentYear - birthdayYear) == YEAR_OF_MAJORITY && (currentMonth - birthdayMonth) == 0 && (currentDay - birthdayDay >= 0)) {
+        } else if ((currentYear - birthdayYear) == YEAR_OF_MAJORITY
+                && (currentMonth - birthdayMonth) == 0
+                && (currentDay - birthdayDay >= 0)) {
             System.out.println("Vous êtes majeur.");
-        } else if ((currentYear - birthdayYear < 0)){
+        } else if ((currentYear - birthdayYear < 0)) {
             System.out.println("Vous n'êtes pas encore né. :)");
-        }else {
-            System.out.println("Vous êtes mineur.");
-        }
+        } else {
 
-        int ageInYears = currentYear - birthdayYear;
-        int ageInMonths = currentMonth - birthdayMonth;
-        int ageInDays = currentDay - birthdayDay;
+            // Je vois que c'est faux mais je suis à cours d'idées
+            int yearsRemainingUntilMajority = YEAR_OF_MAJORITY - (currentYear - birthdayYear);
+            int monthsRemainingUntilMajority = currentMonth - birthdayMonth;
+            int daysRemainingUntilMajority = currentDay - birthdayDay;
+            System.out.println("Vous êtes mineur.");
+            System.out.println("Vous serez majeur dans " + yearsRemainingUntilMajority + " ans, " + monthsRemainingUntilMajority + "mois et " + daysRemainingUntilMajority + " jours.");
+
+        }
 //        System.out.println("Vous avez " + ageInYears + " ans, " + ageInMonths + " mois et " + ageInDays + " jours.");
 
         // BONUS 1
