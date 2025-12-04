9e745fd1932f8b82bcac0cc9aa49fc932c0d0da4
fdelabre-accor <frederic.delabre@consulting-for.accor.com>
2025-12-04 17:31:11 +0100

    📝 Exercice : [⭐] Affichez des séquences de nombres avec la boucle while

--- src/javacore/chapter03/loop/exercise/WhileLoopExample.java
+++ src/javacore/chapter03/loop/exercise/WhileLoopExample.java
@@ -12,5 +12,40 @@ public class WhileLoopExample {
             System.out.println("Tour n°: " + loopCounter + " - Hello World!");
             loopCounter++;
         }
+
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("Exercice : [⭐] Affichez des séquences de nombres avec la boucle while");
+        System.out.println("----------------------------------------------------------------------");
+
+        System.out.println("Nombres ascendants: ");
+
+        byte number = 1;
+        byte maxNumber = 10;
+        while (number <= maxNumber) {
+            System.out.println(number);
+            number++;
+        }
+
+        System.out.println("Nombres descendants: ");
+
+        number = 10;
+        byte minNumber = 1;
+        while (number >= minNumber) {
+            System.out.println(number);
+            number--;
+        }
+
+        System.out.println("Nombres pairs ascendants: ");
+        number = 2;
+        maxNumber = 20;
+
+        while (number <= maxNumber) {
+            System.out.println(number);
+            number += 2;
+        }
+
+        // Ici, j'ai essayé d'utiliser le modulo mais je n'ai pas trouvé de solution sans condition. Je pense qu'on peut mieux faire mais je n'ai pas trouvé.
+
+
     }
 }
