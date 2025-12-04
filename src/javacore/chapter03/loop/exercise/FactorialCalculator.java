bb214c0a67f57becf7d2b5498e68a4194e4530f0
fdelabre-accor <frederic.delabre@consulting-for.accor.com>
2025-12-04 20:35:54 +0100

    📝 Exercice : [⭐⭐] Calculez la factorielle d'un nombre

---
+++ src/javacore/chapter03/loop/exercise/FactorialCalculator.java
@@ -0,0 +1,49 @@
+package javacore.chapter03.loop.exercise;
+
+public class FactorialCalculator {
+
+    public static void main(String[] args) {
+
+        long number = 0;
+        long result = 1;
+
+/*        if (number > 0) {
+            while (number >= 1) {
+                result *= number;
+                number--;
+            }
+            System.out.println("Le factoriel est : " + result);
+        } else if (number == 0) {
+                result *= number;
+            System.out.println("Le factoriel est : " + result);
+        } else {
+            System.out.println("Le factoriel d'un nombre négatif n'existe pas. Nombre: " + number);
+        }*/
+
+        // J'ai choisi la structure ci-dessous pour éviter d'avoir à répéter le System.out.println("Le factoriel est : " + result); 2x
+
+        if (number < 0) {
+            System.out.println("Le factoriel d'un nombre négatif n'existe pas. Nombre: " + number);
+        } else {
+            if (number == 0) {
+                result *= number;
+            } else {
+                while (number >= 1) {
+                    result *= number;
+                    number--;
+                }
+            }
+            System.out.println("Le factoriel est : " + result);
+        }
+
+/*          7 => 5040
+            9 => 362880
+            11 => 39916800
+            20 => 2432902008176640000
+            21 => -4249290049419214848
+
+            A partir de 21, on obtient un résultat négatif, j'imagine parce qu'on dépasse la limite tu type long.
+                */
+    }
+
+}
