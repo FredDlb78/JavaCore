dd9a7fde868f3737d0a5fed9c7b4ae44aeb81320
fdelabre-accor <frederic.delabre783@gmail.com>
2025-12-07 08:25:34 +0100

    📝 Exercice : [⭐⭐⭐] Recherchez les Nombres Premiers (Version 1) 2eme boucle

--- src/javacore/chapter03/loop/exercise/PrimeNumberSearcherV1.java
+++ src/javacore/chapter03/loop/exercise/PrimeNumberSearcherV1.java
@@ -5,18 +5,21 @@ public class PrimeNumberSearcherV1 {
     public static void main(String[] args) {
 
 
-        int number = 17;
+        int number = 100;
         int divisor = 2;
+        int i = 0;
 
-        for (divisor = 2; number % divisor != 0; divisor++) {
-            System.out.println(divisor + " n'est pas le plus petit diviseur de " + number + ".");
+        for (i = 1; i <= number; i++) {
+            //System.out.println("Le nombre testé est : " + i);
+            for (divisor = 2; i % divisor != 0; divisor++) {
+                //System.out.println(divisor + " n'est pas le plus petit diviseur de " + number + ".");
+            }
+            //System.out.println("Le plus petit diviseur de " + i + " est : " + divisor + ".");
+
+            if (i == divisor) {
+                System.out.println(i + " est un nombre premier.");
+            }
         }
-        System.out.println("Le plus petit diviseur de " + number + " est : " + divisor + ".");
-
-        if (number == divisor) {
-            System.out.println(number + " est un nombre premier.");
-        }
-
+        System.out.println("Il y'a 25 nombres premiers entre 1 et 100.");
     }
-
 }
