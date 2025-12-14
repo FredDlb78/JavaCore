6447dfea6e3719e2fae2f43c164dc1057d95b209
fdelabre-accor <frederic.delabre783@gmail.com>
2025-12-14 21:51:24 +0100

    📝 Exercice : [⭐⭐⭐] Affichez la suite de Fibonacci

--- src/javacore/chapter03/loop/exercise/FibonacciCalculator.java
+++ src/javacore/chapter03/loop/exercise/FibonacciCalculator.java
@@ -3,18 +3,44 @@ package javacore.chapter03.loop.exercise;
 public class FibonacciCalculator {
     public static void main(String[] args) {
 
-        int previousValue = 0;
-        int value = 1;
-
         // Suite: 0 1 1 2 3 5 8 13 21
 
-        for (int i = 2; i <= 50; i++) {
-            System.out.println(i);
-            i += (i -1);
-            //System.out.println(i);
+        long previousFibonacciNumber = 0;
+        long fibonacciNumber = 1;
+        long result;
+
+        System.out.println("-----");
+        System.out.println("---Implémentez le programme qui calculera et affichera les 50 premiers termes de la suite de Fibonacci.---");
+        System.out.println("-----");
+
+        for (int count = 0; count < 50; count++) {
+            System.out.println(previousFibonacciNumber);
+            result = fibonacciNumber + previousFibonacciNumber;
+            previousFibonacciNumber = fibonacciNumber;
+            fibonacciNumber = result;
         }
 
+        System.out.println("-----");
+        System.out.println("---BONUS [⭐⭐⭐] : Modifiez votre programme de sorte à ce qu'il affiche les 1000 premiers termes de la suite de Fibonacci, tout en distinguant les termes dont les valeurs sont paires.---");
+        System.out.println("-----");
 
+        int count;
+        int sumOfPairsFibonacciNumbers = 0;
+
+        for (count = 1; count <= 1000; count++) {
+            if (previousFibonacciNumber % 2 == 0) {
+                System.out.println("Nombre pair de la suite de Fibonacci : " + previousFibonacciNumber);
+                sumOfPairsFibonacciNumbers += 1;
+            }
+            else {
+                System.out.println("Nombre impair de la suite de Fibonacci : " + previousFibonacciNumber);
+            }
+                result = fibonacciNumber + previousFibonacciNumber;
+                previousFibonacciNumber = fibonacciNumber;
+                fibonacciNumber = result;
+
+        }
+        System.out.println("Il y'a " + sumOfPairsFibonacciNumbers + " nombres pairs dans la suite de Fibanacci parmi les " + (count - 1) + " premiers.");
 
     }
 }
