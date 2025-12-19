e0ea6a32f0a83ef88d768d01cb60763a4bf724d8
fdelabre-accor <frederic.delabre783@gmail.com>
2025-12-19 13:09:17 +0100

    📝 Exercice : [⭐⭐] Recherchez les Nombres Premiers (Version 3)

---
+++ src/javacore/chapter04/function/exercise/PrimeNumberSearcherV3.java
@@ -0,0 +1,36 @@
+package javacore.chapter04.function.exercise;
+
+public class PrimeNumberSearcherV3 {
+
+    public static void main (String[] args) {
+
+        int potentialPrimeNumber = 0;
+        int primeNumberFound = 0;
+        int searchAmount = 50;
+
+        while (primeNumberFound < searchAmount) {
+
+            if (isPrimeNumber(potentialPrimeNumber)) {
+                System.out.println(potentialPrimeNumber + " est le nombre premier n° " + (primeNumberFound + 1));
+                primeNumberFound++;
+            }
+            potentialPrimeNumber++;
+
+        }
+
+    }
+
+    public static boolean isPrimeNumber(int number) {
+
+        for (int potentialDivisor = 2; potentialDivisor <= number - 1; potentialDivisor++) {
+
+            if (number % potentialDivisor == 0) {
+                return false;
+            } // else { return true } => missing return statement
+        }
+        return true;
+    }
+
+
+
+}
