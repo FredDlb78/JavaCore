4420ee6ac0014ba530ffc07b072059f227625efb
fdelabre-accor <frederic.delabre783@gmail.com>
2026-01-20 22:50:50 +0100

    📝 Exercice : [⭐] Analysez des données pour créer une Classe

---
+++ src/javacore/chapter05/object/exercise/Pen.java
@@ -0,0 +1,38 @@
+package javacore.chapter05.object.exercise;
+
+public class Pen {
+
+    public static void main(String[] args) {
+
+        Pen pen = new Pen();
+        pen.name = "Stylo";
+        pen.color = "bleu";
+        pen.manufacturer = "Bic";
+        pen.quantity = 10;
+        pen.weight = 15;
+        pen.price = 2.5;
+
+        pen.displayInformation();
+
+    }
+
+    String name;
+    String color;
+    String manufacturer;
+
+    int quantity;
+    int weight;
+
+    double price;
+
+    public void displayInformation() {
+        System.out.println(
+                        "Nom: "      + name         + ", "  +
+                        "Couleur: "  + color        + ", "  +
+                        "Prix: "     + price        + "€, " +
+                        "Quantité: " + quantity     + ", "  +
+                        "Fabricant: "+ manufacturer + ", "  +
+                        "Poids: "    + weight       + "g"
+        );
+    }
+}
