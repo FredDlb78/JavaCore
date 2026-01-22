9114a41aac3c73cbd76086140716036711b87f29
fdelabre-accor <frederic.delabre783@gmail.com>
2026-01-22 23:18:13 +0100

    📝 Exercice : [⭐⭐] Implémentez un Chronomètre (Version 3)

--- src/javacore/chapter05/object/exercise/BasicStopwatchV2.java
+++ src/javacore/chapter05/object/exercise/BasicStopwatchV2.java
@@ -2,48 +2,56 @@ package javacore.chapter05.object.exercise;
 
 public class BasicStopwatchV2 {
 
+    String name;
+
     int seconds;
     int minutes;
     int hours;
+
+    public BasicStopwatchV2(String name) {
+        this.name    = name;
+        this.hours   = 0;
+        this.minutes = 0;
+        this.seconds = 0;
+    }
 
     public static void waitOneSecond() throws InterruptedException {
         Thread.sleep(1000);
     }
 
-    // J'ai utilisé un paramètre pour arreter le chrono comme dans la v1 mais si je le retire je peux faire comme dans l'énoncé.
     public void launch(int secondsToElapse) throws InterruptedException {
         int i = 0;
         while (i <= secondsToElapse) {
 
             waitOneSecond();
             i++;
-            seconds++;
+            this.seconds++;
 
-            if (seconds >= 60) {
-                minutes++;
-                seconds = 0;
+            if (this.seconds >= 60) {
+                this.minutes++;
+                this.seconds = 0;
 
-                if (minutes >= 60) {
-                    hours++;
-                    minutes = 0;
+                if (this.minutes >= 60) {
+                    this.hours++;
+                    this.minutes = 0;
                 }
             }
-            displayTime(hours, minutes, seconds);
+            displayTime();
         }
     }
 
-    public static void displayTime(int hours, int minutes, int seconds) {
-        String displayHours = hours < 10 ? "0" + hours : "" + hours;
-        String displayMinutes = minutes < 10 ? "0" + minutes : "" + minutes;
-        String displaySeconds = seconds < 10 ? "0" + seconds : "" + seconds;
+    public void displayTime() {
+        String displayHours = this.hours < 10 ? "0" + this.hours : "" + this.hours;
+        String displayMinutes = this.minutes < 10 ? "0" + this.minutes : "" + this.minutes;
+        String displaySeconds = this.seconds < 10 ? "0" + this.seconds : "" + this.seconds;
 
-        System.out.println(displayHours + ":" + displayMinutes + ":" + displaySeconds);
+        System.out.println(this.name + " : " + displayHours + ":" + displayMinutes + ":" + displaySeconds);
     }
 
     public static void main(String[] args) throws InterruptedException {
 
-        BasicStopwatchV2 basicStopwatch = new BasicStopwatchV2();
-        basicStopwatch.launch(3600);
+        BasicStopwatchV2 hardBoiledEggStopwatch = new BasicStopwatchV2("Oeufs durs");
+        hardBoiledEggStopwatch.launch(3600);
 
     }
 }
