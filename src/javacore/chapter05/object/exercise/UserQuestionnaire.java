4d312886c2af4570a697fcd3a4444d2c6f3a503b
fdelabre-accor <frederic.delabre783@gmail.com>
2026-01-30 09:39:32 +0100

    📝 Exercice : [⭐⭐] Questionnaire utilisateur v2

--- src/javacore/chapter05/object/exercise/UserQuestionnaire.java
+++ src/javacore/chapter05/object/exercise/UserQuestionnaire.java
@@ -4,61 +4,98 @@ import java.util.Scanner;
 
 public class UserQuestionnaire {
 
-    public static void main (String[] args) {
+    static Scanner sc = new Scanner(System.in);
 
-        double result = 0;
-        String message = "";
-        String userLastName = "";
-        String animalName= "";
-        String hasAnimal = "";
+    static double result = 0;
+    static int age;
 
-        Scanner sc = new Scanner(System.in);
+    static String message = "";
+    static String userFirstName;
+    static String userLastName = "";
+    static String animalName = "";
+    static String hasAnimal = "";
+    static String userWishContinueReply;
 
-        System.out.print("Renseignez votre prénom : ");
-        String userFirstName = sc.nextLine();
+    public static void main(String[] args) {
 
-        System.out.print("Renseignez votre âge : ");
-        byte age = sc.nextByte();
-        sc.nextLine();
-
-        System.out.print("Souhaitez-vous continuer à répondre aux questions suivantes ?");
-        String userWishContinueReply = sc.nextLine();
+        setUserFirstName();
+        setUserAge();
+        setUserResponseForContinueForm();
 
         if (isYes(userWishContinueReply)) {
-            System.out.print("Quel est le résultat de 5/2 ?");
-            result = sc.nextDouble();
-            
-            sc.nextLine();
-
-            System.out.print("Renseignez votre nom de famille : ");
-            userLastName = sc.nextLine();
-
-            System.out.print("Avez vous un animal de compagnie ?");
-            hasAnimal = sc.nextLine();
-            message = isYes(hasAnimal) ? "Vous avez un animal de compagnie." : "Vous n'avez pas d'animal de compagnie.";
+            setResult();
+            setUserLastName();
+            setHasAnimal();
+            setMessage(hasAnimal);
 
             if (isYes(hasAnimal)) {
-                System.out.print("Quel est le nom de votre animal de compagnie ?");
-                animalName = sc.nextLine();
+                setAnimalName();
             }
         } else {
             System.out.println("Questionnaire terminé.");
         }
 
+        displayFormResponses();
+    }
+
+    public static void displayFormResponses() {
         System.out.println("Votre prénom est : " + userFirstName);
         System.out.println("Votre âge est de " + age + " ans.");
-        System.out.println("Souhaitez-vous répondre à la suite du questionnaire ?" +userWishContinueReply);
+        System.out.println("Souhaitez-vous répondre à la suite du questionnaire ? " + userWishContinueReply);
+
         if (isYes(userWishContinueReply)) {
-            System.out.println("Vous avez répondu à 5 /2 = " + result);
+            System.out.println("Vous avez répondu à 5 / 2 = " + result);
             System.out.println("Votre nom est : " + userLastName);
             System.out.println(message);
+
             if (isYes(hasAnimal)) {
                 System.out.println("Le nom de votre animal de compagnie est : " + animalName);
             }
         }
     }
 
+    public static void setUserFirstName() {
+        System.out.print("Renseignez votre prénom : ");
+        userFirstName = sc.nextLine();
+    }
+
+    public static void setUserAge() {
+        System.out.print("Renseignez votre âge : ");
+        age = sc.nextInt();
+        sc.nextLine(); // consomme le '\n'
+    }
+
+    public static void setUserResponseForContinueForm() {
+        System.out.print("Souhaitez-vous continuer à répondre aux questions suivantes ? ");
+        userWishContinueReply = sc.nextLine();
+    }
+
+    public static void setResult() {
+        System.out.print("Quel est le résultat de 5/2 ? ");
+        result = sc.nextDouble();
+        sc.nextLine(); // consomme le '\n'
+    }
+
+    public static void setUserLastName() {
+        System.out.print("Renseignez votre nom de famille : ");
+        userLastName = sc.nextLine();
+    }
+
+    public static void setHasAnimal() {
+        System.out.print("Avez vous un animal de compagnie ? ");
+        hasAnimal = sc.nextLine();
+    }
+
+    public static void setMessage(String hasAnimal) {
+        message = isYes(hasAnimal) ? "Vous avez un animal de compagnie." : "Vous n'avez pas d'animal de compagnie.";
+    }
+
+    public static void setAnimalName() {
+        System.out.print("Quel est le nom de votre animal de compagnie ? ");
+        animalName = sc.nextLine();
+    }
+
     public static boolean isYes(String response) {
-        return response.toLowerCase().equals("oui");
+        return response != null && response.trim().equalsIgnoreCase("oui");
     }
 }
