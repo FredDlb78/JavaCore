d1938e939a092294846e3ac8457cbe9d3f664ad2
fdelabre-accor <frederic.delabre783@gmail.com>
2026-02-15 20:55:01 +0100

    🚧 Projet : [⭐⭐⭐⭐] Cryptographie - Chiffrement par substitution Consigne 7

--- src/javacore/chapter05/object/exercise/SubstitutionCipher.java
+++ src/javacore/chapter05/object/exercise/SubstitutionCipher.java
@@ -7,6 +7,9 @@ public class SubstitutionCipher {
     static Scanner scanner = new Scanner(System.in);
     static final short MAX_ATTEMPTS = 5;
 
+    private String alphabet;
+    private String subsAlphabet;
+
     /**
      * Alphabet Standard (Latin)
      */
@@ -16,6 +19,11 @@ public class SubstitutionCipher {
      */
     static String substitutionAlphabet = "ntrhwbgeyjzlsoaqdmcuvfpxik";
 
+    public SubstitutionCipher(String alphabet, String subsAlphabet) {
+        this.alphabet = alphabet;
+        this.subsAlphabet = subsAlphabet;
+    }
+
     public static void main(String[] args) {
 
         /**
@@ -24,6 +32,9 @@ public class SubstitutionCipher {
         String textToEncrypt = "ce message secret ne doit pas arriver entre de mauvaises mains !";
 
         // Ecrivez le code ci-dessous
+
+        SubstitutionCipher cipher = new SubstitutionCipher(latinAlphabet, substitutionAlphabet);
+
 //        System.out.println(encryptText(textToEncrypt));
 //        System.out.println(encryptTextV2(textToEncrypt));
 //        System.out.println(cipher(textToEncrypt, latinAlphabet, substitutionAlphabet));
@@ -32,7 +43,7 @@ public class SubstitutionCipher {
 //        String decryptedText = cipher(encryptedText, substitutionAlphabet, latinAlphabet, 3);
 //        System.out.println(decryptedText);
 //        System.out.println("Le texte après traitement est : " + processText(askShouldEncrypt()));
-        System.out.println("Le texte après traitement est : " + processTextWithUserSubstitutionAlphabet(askShouldEncrypt()));
+        System.out.println("Le texte après traitement est : " + cipher.processTextWithUserSubstitutionAlphabet(askShouldEncrypt()));
     }
 
     // Consigne 6
@@ -150,6 +161,7 @@ public class SubstitutionCipher {
 
     // Consigne 4
     public static String cipher(String textToEncrypt, String alphabet, String substitutionAlphabet, int cipherIterations) {
+        SubstitutionCipher cipher = new SubstitutionCipher(alphabet, substitutionAlphabet);
         String encryptedText = "";
         for (int iteration = 0; iteration < cipherIterations; iteration++) {
             encryptedText = cipher(textToEncrypt, alphabet, substitutionAlphabet);
@@ -160,6 +172,8 @@ public class SubstitutionCipher {
 
     // Consigne 3
     public static String cipher(String textToEncrypt, String alphabet, String substitutionAlphabet) {
+        SubstitutionCipher cipher = new SubstitutionCipher(alphabet, substitutionAlphabet);
+
         String encryptedText = "";
 
         for (int textIndex = 0; textIndex < textToEncrypt.length(); textIndex++) {
