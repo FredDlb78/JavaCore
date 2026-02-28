ec11f029daf4fea659471db9c5bccb26497f9428
fdelabre-accor <frederic.delabre783@gmail.com>
2026-02-28 13:45:42 +0100

    📝 Exercice : [⭐⭐⭐] Analyseur de notes à l'école Consigne 8

--- src/javacore/chapter05/object/exercise/GradesAnalyzer.java
+++ src/javacore/chapter05/object/exercise/GradesAnalyzer.java
@@ -1,27 +1,62 @@
 package javacore.chapter05.object.exercise;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class GradesAnalyzer {
 
-    private static final int MAX_ATTEMPTS = 3;
+    static final int MAX_ATTEMPTS = 3;
+    static final int MIN_GRADES_NUMBER = 2;
+    static final int MAX_GRADES_NUMBER = 30;
+    static final int MIN_GRADE = 0;
+    static final int MAX_GRADE = 20;
+
     static Scanner sc = new Scanner(System.in);
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         int[] gradesArray = setGrades();
+
         int threshold = 12;
         int countAbove = getGradeCountAboveThreshold(gradesArray, threshold);
         double percentAbove = (double) countAbove / gradesArray.length * 100;
         System.out.println("Nombre de notes > " + threshold + " = " + countAbove + " (soit " + percentAbove + "%)");
+
         int from = 12;
         int to = 18;
         int countRange = getGradeCountBetweenRange(gradesArray, from, to);
         double percentRange = (double) countRange / gradesArray.length * 100;
         System.out.println("Nombre de notes de " + from + " à " + to + " : " + countRange + " (soit " + percentRange + "%)");
 
+        System.out.println(Arrays.toString(setGradesBis()));
+
     }
 
     // 8
+    public static int[] setGradesBis() {
+        int[] grades = new int[0];
+        int gradeNumber = 1;
+        boolean isUserEntryDone = false;
+
+        while (!isUserEntryDone && grades.length < MAX_GRADES_NUMBER) {
+            System.out.print("Saisissez votre " + gradeNumber + "e note : ");
+            gradeNumber++;
+            int grade = sc.nextInt();
+
+            if (grade == -1) {
+                isUserEntryDone = true;
+            } else if (grade < MIN_GRADE || grade > MAX_GRADE) {
+                System.out.println("Note invalide, veuillez saisir une valeur entre 0 et 20.");
+            } else {
+                int[] newGrades = new int[grades.length + 1];
+                for (int i = 0; i < grades.length; i++) {
+                    newGrades[i] = grades[i];
+                }
+                newGrades[grades.length] = grade;
+                grades = newGrades;
+            }
+        }
+        return grades;
+    }
 
 
     // 7
@@ -30,7 +65,7 @@ public class GradesAnalyzer {
 
         for (int gradeIndex = 0; gradeIndex < gradesArray.length; gradeIndex++) {
             int currentGrade = gradesArray[gradeIndex];
-            if ( from <= currentGrade  && currentGrade <= to) {
+            if (from <= currentGrade && currentGrade <= to) {
                 count += 1;
             }
         }
@@ -99,14 +134,14 @@ public class GradesAnalyzer {
                 grades[gradeIndex] = sc.nextByte();
                 attempts++;
 
-                if (grades[gradeIndex] < 0 || grades[gradeIndex] > 20) {
+                if (grades[gradeIndex] < MIN_GRADE || grades[gradeIndex] > MAX_GRADE) {
                     System.out.println("Valeur invalide, veuillez saisir une valeur entre 0 et 20 inclu.");
                 } else {
                     break;
                 }
             } while (attempts < MAX_ATTEMPTS);
 
-            if (grades[gradeIndex] < 0 || grades[gradeIndex] > 20) {
+            if (grades[gradeIndex] < MIN_GRADE || grades[gradeIndex] > 20) {
                 System.out.println("Nombre de tentatives maximum atteint. Note mise à 0 par défaut.");
                 grades[gradeIndex] = 0;
             }
@@ -119,12 +154,12 @@ public class GradesAnalyzer {
         int attempts = 0;
         int gradesNumber;
         do {
-            System.out.print("Combien de notes souhaitez-vous saisir ? (Min: 2 - Max: 30) :");
+            System.out.print("Combien de notes souhaitez-vous saisir ? (Min: " + MIN_GRADES_NUMBER + " - Max: " + MAX_GRADES_NUMBER + ") :");
             gradesNumber = sc.nextByte();
             attempts++;
 
-            if (gradesNumber < 2 || gradesNumber > 30) {
-                System.err.println("Valeur invalide, veuillez saisir une valeur entre 2 et 30 inclu.");
+            if (gradesNumber < MIN_GRADES_NUMBER || gradesNumber > MAX_GRADES_NUMBER) {
+                System.err.println("Valeur invalide, veuillez saisir une valeur entre " + MIN_GRADE + " et " + MAX_GRADE + " inclu.");
             } else {
                 break;
             }
